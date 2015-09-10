a1f0b33ae728ec5c2bb4fce856ae8e6793851637
John Murphy <chiefsmurph@gmail.com>
2015-09-09 18:12:16 -0700

    debug the insert

--- server.js
+++ server.js
@@ -80,8 +80,9 @@ app.post('/submit-sig', function(req, res, next) {
         console.log('about to insert');
         var queryText = 'INSERT INTO pledges(flname, email) VALUES($1, $2) RETURNING id'
         client.query(queryText, [req.body.name, req.body.email], function(err, result) {
-          if(!err) {
-            console.log('success!!!');
+          console.log('err' + err);
+          if(result) {
+            console.log('success!!!' + result);
           }
         });
       });
