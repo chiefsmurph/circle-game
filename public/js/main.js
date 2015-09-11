e5b52bece0e3bc09f06e6b64664d1d0ece09041a
John Murphy <chiefsmurph@gmail.com>
2015-09-11 16:07:18 -0700

    Initial commit of express / websockets backend with blank gamearea

--- public/js/main.js
+++ public/js/main.js
@@ -1,123 +1,17 @@
 // establish socket connection
 var socket = io.connect(window.location.hostname + ":" + window.location.port);
-var count = 0;
-var startCount;
-
-// only after the images have loaded and the splash screen fadeout should the dialog
-var el = document.querySelector('#counter');
-
-var od = new Odometer({
-  el: el,
-
-  // Any option (other than auto and selector) can be passed in here
-  theme: 'plaza'
-});
-
-
-// get the current counter
-$.get( "/getCounter", function( data ) {
-  //$('#counter').html(data.count);
-  startCount = data.count;
-  console.log(data);
-});
 
 // setup counter watching socket
 socket.on('status', function (data) {
-  $('#counter').html(data.count);
+  
 });
-
-
-$(window).load(function() {
-
-  $('#splashscreen').fadeOut(500, function() {
-
-    $('#bg').show();
-    $('.container').fadeIn(500, function() {
-
-          $('#counter').html(startCount);
-
-    });
-  });
-
-})
-
-
 
 
 $(function() {
 
   // ONCE THE DOM HAS LOADED...
 
-  // first off load the images for the page
-
-
-
-
   // FIRST OFF INITIALIZATIONS
-
-  $( "#dialog-form" ).show();
-
-  // jquery dialog
-  dialog = $( "#dialog-form" ).dialog({
-    autoOpen: false,
-    height: 440,
-    width: 270,
-    modal: true,
-    // open: function(event, ui) {
-    //   $( "#dialog-form" ).css('overflow', 'hidden');
-    // },
-    buttons: {
-      "Sign the pledge": function() {
-
-          var tosend = {
-            name: $('#name').val(),
-            email: $('#email').val()
-          };
-
-          if (validateForm()) {
-
-            $.post( '/submit-sig', tosend, function(data) {
-
-                 $('#addone').fadeOut('slow', function() {
-                   if (data.error) {
-                     $('#status-panel').html(data.error);
-                   } else {
-                     docCookies.setItem('voted', 'alphabet');
-                     $('#status-panel').html(data.response);
-                   }
-                 });
-                 console.log(data.response);
-               },
-               'json' // I expect a JSON response
-            );
-
-            dialog.dialog( "close" );
-
-        }
-
-      },
-      Cancel: function() {
-        dialog.dialog( "close" );
-      }
-    },
-    close: function() {
-      $('form#new-sig').find("input[type=text], textarea").val("");
-      $('form#new-sig').find("input[type=text], textarea").removeClass( "ui-state-error" );
-    }
-  });
-
-  // check if they have already pledged
-
-  if (docCookies.getItem('voted')) {
-    $('#status-panel').html('Thank you for standing up to the Don and making your voice heard.');
-  }
-
-  // and "pledge" button event handler
-
-  $('#addone').click(function() {
-    this.blur();
-    dialog.dialog( "open" );
-  });
 
   //END INITS
 
--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-console.log('DB URL!!! ' + process.env.DATABASE_URL);
-
-var pg = require('pg');
-var fs = require('fs');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -22,106 +18,9 @@ app.use(express.static(__dirname + '/public'));
 
 
 var count = 0;
-var emailFile = 'email-list.txt';
-var emailList = [];
-
-// setup db
-
-
-/*
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('ALTER TABLE pledges ADD COLUMN party varchar(250) NOT NULL DEFAULT "null"');
-  console.log('adding pledge col');
-  query.on('row', function(row) {
-    console.log('row: ' + JSON.stringify(row));
-  });
-});
-*/
-
-// get current count
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM pledges', function(err, result) {
-
-
-    for (var i = 0; i < result.rows.length; i++) {
-      emailList.push(result.rows[i].email.toLowerCase());
-      count++;
-    }
-
-    console.log('current count: ' + count);
-    console.log('current emails: ' + emailList);
-
-
-
-  });
-
-
-});
-
-
-// fs.readFileSync(emailFile).toString().split('\n').forEach(function (line) {
-//   emailList.push(line.split(',')[0].toLowerCase());
-// });
-//
-// count = emailList.length - 1;
-
-app.get('/getCounter', function(req, res, next) {
-
-  res.json({count: count});
-
-});
-
-app.get('/getsigs', function(req, res, next) {
-
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    var query = client.query('SELECT * FROM pledges', function(err, result) {
-      res.send(JSON.stringify(result.rows));
-    });
-
-
-  });
-
-});
-
-app.post('/submit-sig', function(req, res, next) {
-
-
-
-  console.log(req.body);
-
-  if (emailList.indexOf(req.body.email.toLowerCase()) > -1) {
-
-    res.json({error: 'sorry that email has already been submitted'});
-
-  } else if (!req.body.email.match(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/) ) {
-
-    res.json({error: 'dont be such a n00b.'});
-
-  } else {
-
-      pg.connect(process.env.DATABASE_URL, function(err, client) {
-        console.log('about to insert');
-        var queryText = 'INSERT INTO pledges(fsname, email) VALUES($1, $2)'
-        client.query(queryText, [req.body.name, req.body.email], function(err, result) {
-          console.log('err' + err);
-          if(result) {
-            console.log('success!!!' + result);
-
-            count++;
-            res.json({response: 'thank you!  your pledge has been received.'});
-            setTimeout(function() {
-              io.sockets.emit('status', { count: count });
-            }, 1000);
-          }
-        });
-      });
-
-  }
-
-
-
-});
 
 io.sockets.on('connection', function (socket) {
-  io.sockets.emit('status', { count: count }); // note the use of io.sockets to emit but socket.on to listen
+  setTimeout(function() {
+    io.sockets.emit('status', { count: count }); // note the use of io.sockets to emit but socket.on to listen
+  }, 2000);
 });
