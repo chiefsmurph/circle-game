055a561350027b87958e6a7883a989032a57ad53
John Murphy <chiefsmurph@gmail.com>
2015-09-16 15:34:24 -0700

    further console logs for client receiving scores

--- public/js/main.js
+++ public/js/main.js
@@ -187,6 +187,7 @@ var backToRoomChooser = function() {
 
 socket.on('highScores', function(data) {
 
+  console.log('received high scores');
   $('#highScorePanel tbody').empty();
   highScoreData = data.scoreArr;
   for (var i = 0; i < highScoreData.length; i++) {
