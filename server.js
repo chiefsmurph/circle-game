2b940348cd21352aae01d806a19ee04670f9a6c2
John Murphy <chiefsmurph@gmail.com>
2015-09-17 13:57:57 -0700

    found typo

--- server.js
+++ server.js
@@ -345,7 +345,7 @@ var newRoom = function(roomName) {
         console.log('allcols' + allColors + ' and ' + possibleColors);
         var takenColors = new Array;
         for (var user in rooms[roomName].userBank) {
-          takenColors.push(rooms[roomName].user[color]);
+          takenColors.push(rooms[roomName].userBank[color]);
         }
         console.log('takencols' + JSON.stringify(takenColors));
         var remainingColors = allColors.filter(function(i) {
