5f7e995ffe466dd9477b4cb9805d8eb4f57f1abf
John Murphy <chiefsmurph@gmail.com>
2016-03-16 23:17:33 -0700

    more

--- server.js
+++ server.js
@@ -943,6 +943,8 @@ function findWithAttr(array, attr, value) {
     for(var i = 0; i < array.length; i += 1) {
         if(array[i][attr] === value) {
             return i;
+        } else {
+          console.log(array[i][attr], 'doesnt equal ' + value);
         }
     }
     return null;
